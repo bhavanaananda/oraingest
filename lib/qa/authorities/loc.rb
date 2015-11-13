f74bd0ccf9b47e9df53e77be078a3d964655cb19
Bhavana Ananda <bhavana.kaushik@gmail.com>
2015-11-13 16:22:53 +0000

    Separated the get_json function needed by all three cud, fast and loc rb files into web_service.rb. and inherited the WebService class in all the files. This resolves the dependency and 500 error is eleminated.

--- lib/qa/authorities/loc.rb
+++ lib/qa/authorities/loc.rb
@@ -1,7 +1,7 @@
 require 'uri'
 
 module Qa::Authorities
-  class Loc < WebServiceBase
+  class Loc < Qa::Authorities::WebService
 
     # Initialze the Loc class with a query and get the http response from LOC's server.
     # This is set to a JSON object
